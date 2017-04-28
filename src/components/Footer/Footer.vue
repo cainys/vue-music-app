e619f457bb1a9fa6e28fffe9cd1f7493e6adea4f
cainys <1018635847@qq.com>
2017-04-28 17:30:13 +0800

    add svg

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="footer" v-show="isShowMiniMusic" :style="{backgroundColor:skinColor}">
+        <div class="mini-music">
+            <div class="music-img">
+                <img @click="" ref="img" :src="audio.musicImgSrc || (musicData[0] && musicData[0].musicImgSrc) || defaultImg" alt="img"  />
+            </div>
+            <div class="music-name">
+                <p>{{audio.name || (musicData[0] && musicData[0].name)}}</p>
+                <div class="process">
+                    <span class="start"></span>
+                    <div class="process-bar" ref="processBar" @click="" @touchmove="" @touchend="">
+                        <div ref="now" class="now" :style="{width:(now / native.duration).tofixed(3)*100+'%'}"></div>
+                    </div>
+                    <span class="end">{{totalTime}}</span>
+                </div>
+            </div>
+            <div class="music-control"></div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default{
+    computed: {
+        ...mapState(['isPlaying', 'isShowAsideMenu', 'isShowMiniMusic', 'audio', 'DOM', 'musicData', 'skinColor'])
+    },
+    mounted () {
+        this.nativeAudio = document.querySelector('audio')
+        this.nativeAudio.addEventListener('play', () => {
+        this.totalTime = this.transformTime(this.nativeAudio.duration)
+        this.now = this.nativeAudio.currentTime
+        setInterval(() => {
+            this.now = this.nativeAudio.currentTime
+        }, 1000)
+        })
+    },
+    data () {
+    return {
+        playIcon: 'play-icon',
+        pauseIcon: 'pause-icon',
+        now: 0,
+        nativeAudio: {},
+        totalTime: '0:00',
+        defaultImg: 'https://microzz.com/img/avatar.jpg'
+        }
+  },
+  methods: {
+    play () {
+      this.$store.commit('play', !this.isPlaying)
+      !this.isPlaying ? this.DOM.audio.pause() : this.DOM.audio.play()
+    },
+    showPlay () {
+      if (this.isShowAsideMenu) {
+        return
+      }
+      this.$store.commit('showIndex', false)
+      this.$store.commit('showMiniMusic', false)
+    },
+    changeTime (event) {
+      let progressBar = this.$refs.progressBar
+      let coordStart = progressBar.getBoundingClientRect().left
+      let coordEnd = event.pageX
+      this.nativeAudio.currentTime = (coordEnd - coordStart) / progressBar.offsetWidth * this.nativeAudio.duration
+      this.now = this.nativeAudio.currentTime
+      this.nativeAudio.play()
+      this.$store.commit('play', true)
+    },
+    touchMove (event) {
+      let progressBar = this.$refs.progressBar
+      let coordStart = progressBar.getBoundingClientRect().left
+      let coordEnd = event.touches[0].pageX
+      this.$refs.now.style.width = ((coordEnd - coordStart) / progressBar.offsetWidth).toFixed(3) * 100 + '%'
+    },
+    touchEnd (event) {
+      this.nativeAudio.currentTime = this.$refs.now.style.width.replace('%', '') / 100 * this.nativeAudio.duration
+      this.now = this.nativeAudio.currentTime
+      this.nativeAudio.play()
+      this.$store.commit('play', true)
+    },
+    transformTime (seconds) {
+      let m, s
+      m = Math.floor(seconds / 60)
+      m = m.toString().length === 1 ? ('0' + m) : m
+      s = Math.floor(seconds - 60 * m)
+      s = s.toString().length === 1 ? ('0' + s) : s
+      return m + ':' + s
+    }
+
+  }
+  }
+</script>
+<style lang="scss" scoped>
+.fade-enter-active {
+  transition: all .3s ease-in-out;
+}
+.fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.fade-enter, .fade-leave-active {
+  transform: translateY(50px);
+  opacity: 0;
+}
+  .footer {
+    background: #B72712;
+    width: 100%;
+    height: 70px;
+    text-align: center;
+    // position: fixed;
+    // bottom: 0;
+
+    .mini-music {
+      display: flex;
+      height: 70px;
+      line-height: 70px;
+
+      .music-img {
+        width: 70px;
+        height: 70px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+          width: 70px;
+          height: 70px;
+          border-radius: 35px;
+          background-size: contain;
+          cursor: pointer;
+        }
+      }
+      .music-name {
+        flex: 5;
+        color: white;
+        overflow: hidden;
+        position: relative;
+        width: 100%;
+
+        p {
+          position: relative;
+          z-index: 1;
+          height: 40px;
+          line-height: 40px;
+          overflow: hidden;
+          white-space: nowrap;
+          // cursor: pointer;
+        }
+        .progress {
+          position: absolute;
+          width: 100%;
+          top: 10px;
+          left: 0;
+          span.start {
+            position: absolute;
+            left: 6px;
+            // padding-right: 5px;
+          }
+          span.end {
+            position: absolute;
+            right: 4px;
+            // padding-left: 5px;
+          }
+          @media screen and(min-width: 600px) {
+            span.start {
+              position: absolute;
+              left: 26px;
+            }
+            span.end {
+              position: absolute;
+              right: 30px;
+            }
+          }
+          .progress-bar {
+            position: relative;
+            width: 50%;
+            height: 5px;
+            display: inline-block;
+            background-color: rgba(255, 255, 255, .5);
+            vertical-align: 2px;
+            border-radius: 3px;
+            cursor: pointer;
+
+            .now {
+              position: absolute;
+              left: 0;
+              display: inline-block;
+              max-width: 100%;
+              // width: 70%;
+              height: 5px;
+              background-color: #31c27c;
+            }
+            .now::after {
+              content: "";
+              position: absolute;
+              left: 100%;
+              width: 2px;
+              height: 6px;
+              background-color: white;
+            }
+          }
+        }
+      }
+      .music-control {
+        flex: 1.5;
+        i {
+          padding-right: 10px;
+          width: 45px;
+          height: 45px;
+          margin-top: 13px;
+          display: inline-block;
+          cursor: pointer;
+          border-radius: 22px;
+        }
+        .play-icon {
+          background: url(./play.svg) no-repeat;
+          background-size: contain;
+
+        }
+        .pause-icon {
+          background: url(./pause.svg) no-repeat;
+          background-size: contain;
+        }
+      }
+    }
+  }
+</style>
