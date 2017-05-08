04ef2b84f2d743e55a1511b493747be16429caaf
gsy <1018635847@qq.com>
2017-05-08 23:18:13 +0800

    add music control

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <side-menu></side-menu>
-    <router-view></router-view>
+    <!--主界面-->
+    <transition name="show">
+      <div v-show="isShowIndex">
+        <v-header></v-header>
+        <side-menu></side-menu>
+        <router-view></router-view>
+        <v-footer></v-footer>
+      </div>
+    </transition>
+
+    <audio v-bind:src="audio.src || (musicData[0]&&musicData[0].src) || defaultSrc" v-bind:autoplay="isPlaying" ref="audio"></audio>
   </div>
 </template>
 
 <script>
 import VHeader from '@/components/Header/Header'
 import SideMenu from '@/components/AsideMenu/AsideMenu'
+import VFooter from '@/components/Footer/Footer'
+import { mapState } from 'vuex'
 export default {
   name: 'app',
   components: {
-    VHeader, SideMenu
+    VHeader, SideMenu, VFooter
+  },
+  computed: {
+    ...mapState(['isShowIndex', 'audio', 'musicData', 'isPlaying'])
+  },
+  data () {
+    return {
+      defaultSrc: 'http://m2.music.126.net/K1SFXCvWf8BO9VEpSvx2ew==/7967061257205150.mp3'
+    }
   },
   beforeCreate () {
     this.$store.dispatch('getData')
+  },
+  mounted () {
+    this.$store.commit('findDOM', {name: 'audio', dom: this.$refs.audio})
   }
 }
 </script>
 
 <style lang='scss'>
 @import './common/style/base.scss';
-
+.show-enter-active {
+  transition: all .4s ease;
+}
+.show-leave-active {
+  transition: all 0 ease-out;
+}
+.show-enter, .show-leave-active {
+  transform: translateX(-350px);
+  opacity: 0;
+}
 #app{
   position:relative;
   height:100%;
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -12,7 +12,7 @@ const state = {
     isShowAsideMenu: false,
     isPlaying: false,
     isAnimation: false,
-    isShowMiniMusic: false,
+    isShowMiniMusic: true,
     isShowAbout: false,
     linkBorderIndex: 0,
     DOM: {},
@@ -40,6 +40,12 @@ const mutations = {
         state.audio.src = state.musicData[index].src
         state.audio.musicImgSrc = state.musicData[index].musicImgSrc
         state.audio.index = state.musicData[index].index
+    },
+    play (state) {
+        state.isPlaying = !state.isPlaying
+    },
+    findDOM (state, payload) {
+        state.DOM[payload.name] = payload.dom
     }
 }
 const actions = {
